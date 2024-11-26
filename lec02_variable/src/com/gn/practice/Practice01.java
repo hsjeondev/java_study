ab0649309ae9e960e6e34605fb13e5a40d6db9ac
hsjeondev <hsjeondev@gmail.com>
2024-11-26 16:16:29 +0900

    [jhs] 구분 문자열 수정

--- lec02_variable/src/com/gn/practice/Practice01.java
+++ lec02_variable/src/com/gn/practice/Practice01.java
@@ -4,16 +4,15 @@ public class Practice01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("기존 코드");
+		System.out.println("변수 사용 전 코드");
 		System.out.println("100 + 10 = " + (100 + 10));
 		System.out.println("10 * (100 + 10) = " + (10 * (100 + 10)));
 		System.out.println("10 * (100 + 10) / 10 = " + (10 * (100 + 10) / 10));
 		System.out.println();
 		
-		System.out.println("기존 코드");
+		System.out.println("변수 사용 후 코드");
 		int x = 10;
 		int y = 100;
-		
 		System.out.println("x = " + x);
 		System.out.println("y = " + y);
 		System.out.println("y + x = " + (y + x));
