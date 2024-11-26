fb6a3522c8d722c299a474ad43263af7c7051634
hsjeondev <hsjeondev@gmail.com>
2024-11-26 12:29:17 +0900

    [jhs] 최초 commit
    	- 표준출력
    	- 이스케이프 문자

---
+++ lec01_basic/src/com/gn/practice/Introduce.java
@@ -0,0 +1,24 @@
+package com.gn.practice;
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		
+		System.out.print("안녕하세요~\n");
+		System.out.print("저는 \"김철수\"입니다.\n");
+		System.out.print("나이는 20살이고, mbti는 istp입니다.\n");
+		System.out.print("\n");
+		
+		System.out.println("안녕하세요~");
+		System.out.println("저는 \"김철수\"입니다.");
+		System.out.println("나이는 20살이고, mbti는 istp입니다.");
+		System.out.println();
+		
+		System.out.printf("안녕하세요~\n");
+		System.out.printf("저는 \"%s\"입니다.\n", "김철수");
+		System.out.printf("나이는 %d살이고, mbti는 %s입니다.", 20, "istp");
+		System.out.printf("\n");
+		
+	}
+
+}
