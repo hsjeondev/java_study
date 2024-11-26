23a0a8b3373d7a44740d1764c7777987a602a857
hsjeondev <hsjeondev@gmail.com>
2024-11-26 15:24:13 +0900

    [jhs] 변수 과제
    	- 변수 선언 및 초기화
    	- 변수 출력

---
+++ lec02_variable/src/com/gn/practice/Practice01.java
@@ -0,0 +1,25 @@
+package com.gn.practice;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("기존 코드");
+		System.out.println("100 + 10 = " + (100 + 10));
+		System.out.println("10 * (100 + 10) = " + (10 * (100 + 10)));
+		System.out.println("10 * (100 + 10) / 10 = " + (10 * (100 + 10) / 10));
+		System.out.println();
+		
+		System.out.println("기존 코드");
+		int x = 10;
+		int y = 100;
+		
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("y + x = " + (y + x));
+		System.out.println("x * (y + x) = " + (x * (y + x)));
+		System.out.println("x * (y + x) / x = " + (x * (y + x) / x));
+
+	}
+
+}
--- lec02_variable/src/com/gn/study/VariableBasic.java
+++ lec02_variable/src/com/gn/study/VariableBasic.java
@@ -3,6 +3,19 @@ package com.gn.study;
 public class VariableBasic {
 	// ctrl + space -> "main"
 	public static void main(String[] args) {
+		// 1. 변수 선언
+		int number;
+		// 2. 변수 초기화
+		int age;
+		age = 100;
+		int age2 = 100;
+		//3. 변수 표준출력
+		System.out.println(age);
+		//4. 변수의 재할당
+		int score = 90;
+		System.out.println("1 : " + score);
+		score = 95;
+		System.out.println("2 : " + score);
 		
 	}
 
