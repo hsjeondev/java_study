f14a8d8a0f4e1f799ec52d9dba88a25b08a8f033
hsjeondev <hsjeondev@gmail.com>
2024-11-27 15:41:24 +0900

    [jhs] 복합 대입 연산자

---
+++ lec03_operator/src/com/gn/practice/Practice02.java
@@ -0,0 +1,52 @@
+package com.gn.practice;
+
+import java.util.Scanner;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력해주세요 : ");
+		int result = scanner.nextInt();
+		int originalResult = result;
+		System.out.println();
+		
+//		System.out.println("입력한 숫자 " + result + "을(를) 복합 대입 연산 한 결과:");
+//		result += 3;
+//		System.out.println(originalResult + " + 3 = " + result);
+//		originalResult = result;
+//		result -= 10;
+//		System.out.println(originalResult + " - 10 = " + result);
+//		originalResult = result;
+//		result *= 2;
+//		System.out.println(originalResult + " * 2 = " + result);
+//		originalResult = result;
+//		result /= 3;
+//		System.out.println(originalResult + "을 3으로 나눈 후 몫 : " + result);
+//		originalResult = result;
+//		result %= 6;
+//		System.out.println(originalResult + "을 6으로 나눈 후 나머지 : " + result);
+		
+		System.out.println("입력한 숫자 " + result + "을(를) 복합 대입 연산 한 결과:");
+		System.out.print(result);
+		result += 3;
+		System.out.println(" + 3 = " + result);
+		System.out.print(result);
+		result -= 10;
+		System.out.println(" - 10 = " + result);
+		System.out.print(result);
+		result *= 2;
+		System.out.println(" * 2 = " + result);
+		System.out.print(result);
+		result /= 3;
+		System.out.println("을 3으로 나눈 후 몫 : " + result);
+		System.out.print(result);
+		result %= 6;
+		System.out.println("을 6으로 나눈 후 나머지 : " + result);
+
+		scanner.close();
+	}
+
+}
--- lec03_operator/src/com/gn/study/ArithmeticOperator.java
+++ lec03_operator/src/com/gn/study/ArithmeticOperator.java
@@ -14,6 +14,23 @@ public class ArithmeticOperator {
 		System.out.println(su1 % su2);
 
 		// 2. 복합 대입 연산자
+		int a = 5;
+//		a = a + 3;
+		a += 3;
+		System.out.println(a);
+		
+		int b = 10;
+		b -= 4;
+		// b -> 6
+		b *= 5;
+		System.out.println(b);
+		
+		int c = 10;
+		c /= 3;
+		
+		int d = 10;
+		d %= 3;
+		
 	}
 
 }
