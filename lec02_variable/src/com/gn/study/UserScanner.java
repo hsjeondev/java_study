62bff4038cbc59ca7c5507b8e66dd3c5492367af
hsjeondev <hsjeondev@gmail.com>
2024-11-27 14:26:24 +0900

    [jhs] Scanner nextLine 주의사항

--- lec02_variable/src/com/gn/study/UserScanner.java
+++ lec02_variable/src/com/gn/study/UserScanner.java
@@ -8,16 +8,17 @@ public class UserScanner {
 	public static void main(String[] args) {
 		// 2. Scanner 이름짓기
 		Scanner scanner = new Scanner(System.in);
+		// 정수 형태 데이터 입력받기
+		System.out.print("점수 : ");
+		int num = scanner.nextInt();
+		System.out.println("점수 : " + num);
+		scanner.nextLine();
 		// 3. 입력받기
 		System.out.print("이름 : ");
 		String name = scanner.nextLine();
 		
 		System.out.println("이름 : " + name);
 		
-		// 정수 형태 데이터 입력받기
-		System.out.print("점수 : ");
-		int num = scanner.nextInt();
-		System.out.println("점수 : " + num);
 	}
 
 }
