3195f08c83237eddfd4f4714306a626d669d063c
JeonHongShik <hongshik98@naver.com>
2024-12-02 21:52:49 +0900

    [jhs] 반복문 과제 완료

--- lec04_control/src/com/gn/homework/loop/Practice.java
+++ lec04_control/src/com/gn/homework/loop/Practice.java
@@ -3,9 +3,38 @@ package com.gn.homework.loop;
 import java.util.Scanner;
 
 public class Practice {
+	
+	public void practice01() { // 1~50까지 짝수 합하기
+		Scanner scanner = new Scanner(System.in);
+		int inputNumber = 0;
+		int sum = 0;
+		
+		System.out.println("=== 1~50까지 짝수 합하기 ===");
+		while(true) {
+			System.out.print("정수(1~50) : ");
+			inputNumber = scanner.nextInt();
+			
+			if(inputNumber < 1 || inputNumber > 50) {
+				System.out.println("1~50 사이의 정수를 입력하세요.");
+				continue;
+			}
+			
+			
+			for(int i = 2; i <= inputNumber; i+=2) {
+				sum += i;
+			}
+			
+			System.out.println("합계 : " + sum);
+			break;
+			
+		}
+		
+		
+		scanner.close();
+	}
     
     public void practice02() { // 구구단 무한반복
-        
+
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("=== 구구단 무한반복 ===");
@@ -36,7 +65,7 @@ public class Practice {
         
         scanner.close();
     }
-
+    
     public void practice03() { // 탈출합시다.
         Scanner scanner = new Scanner(System.in);
         
@@ -56,6 +85,51 @@ public class Practice {
         }
         
         scanner.close();
+    }
+
+    public void practice04() { // 게임 재시작
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== 게임 재시작 ===");
+        System.out.println("게임을 실행합니다!");
+
+        do {
+            System.out.println("게임을 다시 플레이하시겠습니까?(yes/no)");
+            String message = scanner.next();
+
+            if (message.equals("yes")) {
+                System.out.println("게임을 실행합니다!");
+                continue;
+            } else if (message.equals("no")) {
+                System.out.println("게임을 종료합니다.");
+                break;
+            } else {
+                System.out.println("올바른 입력이 아닙니다.");
+                continue;
+            }
+        } while (true);
+
+        scanner.close();
+    }
+    
+    public void practice05() { // 양수만 더하기
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	int sum = 0;
+    	
+    	System.out.println("양수만 더하기");
+    	while(true) {
+    		System.out.print("숫자(0을 입력하면 종료) : ");
+    		int number = scanner.nextInt();
+    		
+    		if(number == 0) {
+    			break;
+    		} else if(number > 0) {
+    			sum += number;
+    		}
+    	}
+    	
+    	System.out.println("양수의 합계 : " + sum);
     }
 
     public void practice06() { // 트리 만들기
@@ -83,7 +157,7 @@ public class Practice {
         scanner.close();
     }
 
-    public void practice07() { //박수박수박
+    public void practice07() { // 박수박수박
     
     	Scanner scanner = new Scanner(System.in);
     	System.out.println("=== 박수박수박 === ");
@@ -111,4 +185,38 @@ public class Practice {
     	
     	scanner.close();
     }
+
+    public void practice08() { // 문자열의 개수
+        Scanner scanner = new Scanner(System.in);
+        String inputContinue = "";
+
+        System.out.println("=== 문자열 개수 ===");
+        do {
+            System.out.print("문자열 : ");
+            String inputString = scanner.next();
+            System.out.print("문자 : ");
+            char inputLetter = scanner.next().charAt(0);
+
+            int sum = 0;
+            for (int i = 0; i < inputString.length(); i++) {
+                if (inputString.charAt(i) == inputLetter) {
+                    sum++;
+                }
+            }
+            System.out.println("포함된 개수 : " + sum);
+
+            do {
+                System.out.print("다시 입력하시겠습니까? (y/n) : ");
+                inputContinue = scanner.next();
+
+                if (!inputContinue.equalsIgnoreCase("y") && !inputContinue.equalsIgnoreCase("n")) {
+                    System.out.println("잘못된 대답입니다. 다시 입력해주세요.");
+                }
+            } while (!inputContinue.equalsIgnoreCase("y") && !inputContinue.equalsIgnoreCase("n"));
+
+        } while (inputContinue.equalsIgnoreCase("y"));
+
+        scanner.close();
+    }
+
 }
--- lec04_control/src/com/gn/homework/loop/Run.java
+++ lec04_control/src/com/gn/homework/loop/Run.java
@@ -10,17 +10,25 @@ public class Run {
         Practice practice = new Practice();
         int select = 0;
 
-        System.out.println("실행할 기능을 선택하세요.");
+        System.out.println("실행할 기능을 선택하세요.");        
+        System.out.println("1. 1~50까지 짝수 합하기");
         System.out.println("2. 구구단 무한반복");
         System.out.println("3. 탈출합시다.");
+        System.out.println("4. 게임 재시작");
+        System.out.println("5. 양수만 더하기");
         System.out.println("6. 트리 만들기");
         System.out.println("7. 박수박수박");
+        System.out.println("8. 문자열의 개수");
         
         do {
             System.out.print("선택: ");
             select = scanner.nextInt();
             
             switch(select) {
+	            case 1:
+	            	System.out.println();
+	            	practice.practice01();
+	            	break;
                 case 2:
                 	System.out.println();
                     practice.practice02();
@@ -28,6 +36,14 @@ public class Run {
                 case 3:
                 	System.out.println();
                     practice.practice03();
+                    break;
+                case 4:
+                	System.out.println();
+                    practice.practice04();
+                    break;
+                case 5:
+                	System.out.println();
+                    practice.practice05();
                     break;
                 case 6:
                 	System.out.println();
@@ -37,12 +53,16 @@ public class Run {
                 	System.out.println();
                     practice.practice07();
                     break;
+                case 8:
+                	System.out.println();
+                    practice.practice08();
+                    break;
                 default:
                     System.out.println("잘못된 선택입니다. 다시 선택하세요.");
                     break;
             }
             
-        } while(select != 2 && select != 3 && select != 6 && select != 7);
+        } while(select < 1 && select > 8);
 
         scanner.close();
     }
